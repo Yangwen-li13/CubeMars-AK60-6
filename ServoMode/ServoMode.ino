be75b263edaea5cef9504a6b1827d4f268a8c8e4
Lezgin Alimoğlu <lezgin2003@gmail.com>
2024-08-14 18:46:53 +0300

    Update ServoMode.ino

--- ServoMode/ServoMode.ino
+++ ServoMode/ServoMode.ino
@@ -105,7 +105,7 @@ void comm_can_set_origin(uint8_t controller_id, uint8_t set_origin_mode) {
 
 void comm_can_set_pos_spd(uint8_t controller_id, float pos, int16_t spd, int16_t RPA) {
   int32_t send_index = 0;
-  int16_t send_index1 = 0;
+  int16_t send_index1 = 4;
   uint8_t buffer[8];
   buffer_append_int32(buffer, (int32_t)(pos * 10000.0), &send_index);
   buffer_append_int16(buffer, spd, &send_index1);
